2f38a757685131c2707e9097271322a3e5c42256
Jonathan McNamara <jpmcnmr@gmail.com>
2019-05-03 18:34:04 -0600

    added routing

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -9,42 +9,26 @@ import Auth from './auth.jsx';
 
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handlePageChange = this.handlePageChange.bind(this);
-  }
-  handlePageChange(event) {
-    const page = event.target.value;
-    console.log(event.target.value);
-    this.props.changePage(page);
-  }
-
-  render() {
-    const {
-      loggedIn,
-    } = this.props;
-    if (loggedIn === false) {
-      return <Router>
+const App = ({
+  loggedIn
+}) => {
+  if (loggedIn === false) {
+    return <Router>
       <div>
         <Auth loggedIn={loggedIn} />
       </div>
       </Router>
-    } else {
-      return <Router>
+  } else {
+    return <Router>
           <div className={styles.container}>
             <VisualConditionalRenderer />
           </div>
         </Router>
-    }
   }
 }
 
 App.propTypes = {
   loggedIn: PropTypes.bool,
-  page: PropTypes.string,
-  authPage: PropTypes.string,
-  changePage: PropTypes.func,
 };
 
 export default App;
--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
-  NavLink
+  NavLink,
+  Link
 } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from '../css/home.css';
@@ -18,28 +19,36 @@ const Home = (props) => {
         <div className={styles.grid}>
             <header className={styles.box}>
               <h1>Korean_Bash</h1>
-              <NavLink>Stats</NavLink>
-              <a>About</a>
-              <a value='learnPage' onClick={handleChangePage}>Learn</a>
-              <a>Review</a>
+              <NavLink to='/Stats'>Stats</NavLink>
+              <NavLink to='/about'>About</NavLink>
+              <NavLink to='/learn'>Learn</NavLink>
+              <NavLink to='/review'>Review</NavLink>
               <div className={styles.profileContainer}>
                 <div className={styles.profilePic}> <img src={userPic} alt='profilePic' className={styles.profileImage}></img></div>
                 <p>{user}</p>
               </div>
             </header>
             <main className={styles.box}>
-              <div className={styles.learn} value='learn_page' onClick={ () => {return handleChangePage('learn_page')}}>
-                Learn
-              </div>
-              <div className={styles.review} value='review' onClick={ () => {return handleChangePage('review')}}>
-                Review
-              </div >
-              <div value='vocab' onClick={ () => {return handleChangePage('vocab')}}>
-                Vocab
-              </div>
-              <div value='alphabet' onClick={ () => {return handleChangePage('alphabet')}}>
-                Alphabet
-              </div>
+              <Link to='/learn'>
+                <div className={styles.learn} value='learn_page' onClick={ () => {return handleChangePage('learn_page')}}>
+                  Learn
+                </div>
+              </Link>
+              <Link to='/review'>
+                <div className={styles.review}>
+                  Review
+                </div >
+              </Link>
+              <Link to='/vocab'>
+                <div value='vocab' onClick={ () => {return handleChangePage('vocab')}}>
+                  Vocab
+                </div>
+              </Link>
+              <Link to='/alphabet'>
+                <div value='alphabet' onClick={ () => {return handleChangePage('alphabet')}}>
+                  Alphabet
+                </div>
+              </Link>
             </main>
             <aside>
               <div>Next up:
